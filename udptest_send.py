5085c78668f8d8270929ff32cdcb9165ad8d52c6
karthikvvk <vkarthik5002@gmail.com>
2025-12-23 09:02:18 +0530

    workspace clean

--- udptest_send.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", 9000))
-
-while True:
-    data, addr = sock.recvfrom(2048)
-    sock.sendto(data, addr)
